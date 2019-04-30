d0f688c0cd376621865d61cce65edad98d80ae16
Chad Fraser <chad.fraser@mail.mcgill.ca>
2019-04-30 16:03:08 -0600

    Implement IComparable

--- MahjongLogic/TileConstants.cs
+++ MahjongLogic/TileConstants.cs
@@ -4,41 +4,55 @@ using System.Text;
 
 namespace Mahjong
 {
-    public class Suit
+    public class Suit : IComparable<Suit>
     {
-        private Suit(string name, string japaneseName)
+        private Suit(string name, string japaneseName, int sortOrder)
         {
             Name = name;
             JapaneseName = japaneseName;
+            SortOrder = sortOrder;
         }
 
         public string Name { get; }
         public string JapaneseName { get; }
+        public int SortOrder { get; }
 
-        public static readonly Suit Dots = new Suit("Dots", "Pin");
-        public static readonly Suit Bamboo = new Suit("Bamboo", "Sou");
-        public static readonly Suit Characters = new Suit("Characters", "Man");
-        public static readonly Suit Wind = new Suit("Wind", "");
-        public static readonly Suit Dragon = new Suit("Dragon", "");
+        public static readonly Suit Dots = new Suit("Dots", "Pin", 0);
+        public static readonly Suit Bamboo = new Suit("Bamboo", "Sou", 1);
+        public static readonly Suit Characters = new Suit("Characters", "Man", 2);
+        public static readonly Suit Wind = new Suit("Wind", "", 3);
+        public static readonly Suit Dragon = new Suit("Dragon", "", 4);
+
+        public int CompareTo(Suit other)
+        {
+            return SortOrder.CompareTo(other.SortOrder);
+        }
     }
 
-    public class HonorType
+    public class HonorType: IComparable<HonorType>
     {
-        private HonorType(string name, string japaneseName)
+        private HonorType(string name, string japaneseName, int sortOrder)
         {
             Name = name;
             JapaneseName = japaneseName;
+            SortOrder = sortOrder;
         }
 
         public string Name { get; }
         public string JapaneseName { get; }
+        public int SortOrder { get; }
 
-        public static readonly HonorType East = new HonorType("East", "Ton");
-        public static readonly HonorType South = new HonorType("South", "Nan");
-        public static readonly HonorType West = new HonorType("West", "Xia");
-        public static readonly HonorType North = new HonorType("North", "Pei");
-        public static readonly HonorType White = new HonorType("White", "Haku");
-        public static readonly HonorType Green = new HonorType("Green", "Hatsu");
-        public static readonly HonorType Red = new HonorType("Red", "Chun");
+        public static readonly HonorType East = new HonorType("East", "Ton", 0);
+        public static readonly HonorType South = new HonorType("South", "Nan", 1);
+        public static readonly HonorType West = new HonorType("West", "Xia", 2);
+        public static readonly HonorType North = new HonorType("North", "Pei", 3);
+        public static readonly HonorType White = new HonorType("White", "Haku", 4);
+        public static readonly HonorType Green = new HonorType("Green", "Hatsu", 5);
+        public static readonly HonorType Red = new HonorType("Red", "Chun", 6);
+
+        public int CompareTo(HonorType other)
+        {
+            return SortOrder.CompareTo(other.SortOrder);
+        }
     }
 }
