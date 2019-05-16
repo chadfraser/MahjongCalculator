977de5a232639e8a4f50c71f55e67aeb50d36738
Chad Fraser <chad.fraser@mail.mcgill.ca>
2019-05-15 18:46:58 -0600

    Split method into applied and generic

---
+++ MahjongLogic/AppliedHKOSWaitingDistanceFinder.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Fraser.Mahjong
+{
+    public class AppliedHKOSEfficientDrawsFinder : HKOSEfficientDrawsFinder
+    {
+        public AppliedHKOSEfficientDrawsFinder(IEnumerable<Tile> allDistinctTiles) : base(allDistinctTiles)
+        {
+            AllAvailableTiles = TileInstance.AllMainTileInstancesFourOfEachTile;
+        }
+
+        public AppliedHKOSEfficientDrawsFinder() : base()
+        {
+            AllAvailableTiles = TileInstance.AllMainTileInstancesFourOfEachTile;
+        }
+
+        protected IEnumerable<Tile> AllAvailableTiles { get; set; }
+
+        public override int GetEfficientDrawCountWithSeenTiles(IList<Tile> tilesInHand, IList<Tile> seenTiles)
+        {
+            var remainingTiles = AllAvailableTiles.Except(seenTiles);
+
+            var efficientDrawCount = 0;
+            var currentWaitingDistince = WaitingDistanceFinder.GetWaitingDistance(tilesInHand);
+
+            IDictionary<Tile, int> countOfTilesUsedInHand = tilesInHand.GroupBy(x => x)
+                .ToDictionary(g => g.Key, g => g.Count());
+            IDictionary<Tile, int> countOfRemainingTiles = remainingTiles.GroupBy(x => x)
+                .ToDictionary(g => g.Key, g => g.Count());
+
+            foreach (var tile in AllDistinctTiles)
+            {
+                if (!countOfRemainingTiles.ContainsKey(tile))
+                {
+                    continue;
+                }
+                var tilesWithNewTileAdded = new List<Tile>(tilesInHand)
+                {
+                    tile
+                };
+                var newWaitingDistance = WaitingDistanceFinder.GetWaitingDistance(tilesWithNewTileAdded);
+                if (newWaitingDistance < currentWaitingDistince)
+                {
+                    efficientDrawCount += countOfRemainingTiles[tile];
+                }
+            }
+            return efficientDrawCount;
+        }
+    }
+}
---
+++ MahjongLogic/HKOSEfficientDrawsFinder.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Fraser.Mahjong
+{
+    public class HKOSEfficientDrawsFinder : IEfficientDrawsFinder
+    {
+        public HKOSEfficientDrawsFinder(IEnumerable<Tile> allDistinctTiles)
+        {
+            WaitingDistanceFinder = new RegularHandSevenPairsThirteenOrphansWaitingDistanceFinder();
+            AllDistinctTiles = new List<Tile>(allDistinctTiles);
+        }
+
+        public HKOSEfficientDrawsFinder() : this(TileInstance.AllMainTileInstances)
+        {
+        }
+
+        protected IWaitingDistanceFinder WaitingDistanceFinder { get; }
+        protected IEnumerable<Tile> AllDistinctTiles { get; set; }
+
+        public int GetEfficientDrawCount(IList<Tile> tiles)
+        {
+            var efficientDrawCount = 0;
+            var currentWaitingDistince = WaitingDistanceFinder.GetWaitingDistance(tiles);
+
+            IDictionary<Tile, int> countOfTilesUsedInHand = tiles.GroupBy(x => x)
+                .ToDictionary(g => g.Key, g => g.Count());
+
+            foreach (var tile in AllDistinctTiles)
+            {
+                if (countOfTilesUsedInHand.ContainsKey(tile) && countOfTilesUsedInHand[tile] == 4)
+                {
+                    continue;
+                }
+                var tilesWithNewTileAdded = new List<Tile>(tiles)
+                {
+                    tile
+                };
+                var newWaitingDistance = WaitingDistanceFinder.GetWaitingDistance(tilesWithNewTileAdded);
+                if (newWaitingDistance < currentWaitingDistince)
+                {
+                    efficientDrawCount += countOfTilesUsedInHand.ContainsKey(tile) ? 4 - countOfTilesUsedInHand[tile] : 4;
+                }
+            }
+            return efficientDrawCount;
+        }
+
+        public virtual int GetEfficientDrawCountWithSeenTiles(IList<Tile> tilesInHand, IList<Tile> seenTiles)
+        {
+            return GetEfficientDrawCount(tilesInHand);
+        }
+    }
+}
