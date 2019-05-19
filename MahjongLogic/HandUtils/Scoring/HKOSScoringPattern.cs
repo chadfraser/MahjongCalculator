d30fe31f3ae8e2b40b1850ca6b1c91f93d7611e2
Chad Fraser <chad.fraser@mail.mcgill.ca>
2019-05-19 14:28:58 -0600

    Add constant scoring instances

---
+++ MahjongLogic/HandUtils/Scoring/HKOSScoringPattern.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fraser.Mahjong
+{
+    public class HKOSScoringPattern
+    {
+        public HKOSScoringPattern(string name, int value, string description)
+        {
+            Name = name;
+            Value = value;
+            Description = description;
+        }
+
+        public string Name { get; set; }
+
+        public int Value { get; set; }
+
+        public string Description { get; set; }
+
+        //public string Name { get; set; }
+    }
+}
