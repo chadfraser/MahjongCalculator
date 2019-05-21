2e6cd7de7ac13e648823a858b0fb7b2fe9390ddf
Chad Fraser <chad.fraser@mail.mcgill.ca>
2019-05-21 17:20:47 -0600

    Add Turn Action enum

---
+++ MahjongLogic/TurnAction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fraser.Mahjong
+{
+    public enum TurnAction
+    {
+        DeclareWin,
+        Discard,
+        FormSequence,
+        FormTriplet,
+        FormQuad,
+        CheckScores,
+        CheckPatterns,
+        Pass
+    }
+}
